2c1422886622271253da7b2d10418fe5402b70a5
sbond75 <43617712+sbond75@users.noreply.github.com>
2022-04-04 15:21:02 -0500

    Specific convention for opencv images

---
+++ SharedMemoryConvention.cpp
@@ -0,0 +1,17 @@
+#include "SharedMemoryConvention.hpp"
+
+void lockSharedMemoryWithConvention(SharedMemoryConvention* shmem) {
+    uint64_t value = 1;
+    uint64_t refcount = __sync_fetch_and_add(&shmem->refcount, value); // Get the value, then do the operation: `*writePtr += value`
+    while (refcount != 0) {
+        puts("Lock: waiting for refcount");
+        usleep(5 // <--milliseconds
+               *1000); // (Microseconds after conversion)
+        refcount = __sync_fetch_and_sub(&shmem->refcount, value);
+        refcount = __sync_fetch_and_add(&shmem->refcount, value);
+    }
+}
+void unlockSharedMemoryWithConvention(SharedMemoryConvention* shmem) {
+    uint64_t value = 1;
+    uint64_t refcount = __sync_sub_and_fetch(&shmem->refcount, value); // Do the operation, then return the value. Operation is `*writePtr -= value`
+}
---
+++ SharedMemoryConvention.hpp
@@ -0,0 +1,24 @@
+#include <cstdint>
+
+template <int imgWidth, int imgHeight>
+struct SharedMemoryConventionTemplate {
+    static constexpr int imgWidth__ = imgWidth;
+    static constexpr int imgHeight__ = imgHeight;
+    SharedMemoryConventionTemplate() :
+        imgWidth_(imgWidth),
+        imgHeight_(imgHeight)
+    {}
+
+    union {
+        uint64_t frameCounter;
+        char msg[8];
+    };
+    uint64_t refcount;
+    int imgWidth_, imgHeight_;
+    uint8_t mat[imgWidth*imgHeight*3];
+};
+
+using SharedMemoryConvention = SharedMemoryConventionTemplate<640, 480>;
+
+void lockSharedMemoryWithConvention(SharedMemoryConvention* shmem);
+void unlockSharedMemoryWithConvention(SharedMemoryConvention* shmem);
